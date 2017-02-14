8496a032fb2c5ccffa039c29ee32169a9b20c3ef
nkirk <nkirk@redlevelnetworks.com>
2017-02-14 15:20:08 -0500

    init

---
+++ AzureMegaTraining/Models/CacheTestData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AzureMegaTraining.Models
+{
+    public class CacheTestData
+    {
+        public int ID { get; set; }
+        public int MyProperty { get; set; }
+        public string Data { get; set; }
+    }
+}
